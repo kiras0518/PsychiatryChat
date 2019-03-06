b676540e32146c34f9dba6f150174fb7f19fc05e
雨夜 <kiras0518@gmail.com>
2019-03-06 22:45:17 +0800

    init User Model

---
+++ PsychiatryChat/Model/User.swift
@@ -0,0 +1,27 @@
+//
+//  User.swift
+//  PsychiatryChat
+//
+//  Created by YU on 2019/3/6.
+//  Copyright © 2019 ameyo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Firebase
+
+struct User {
+    
+    //Properties
+    let name: String
+    let email: String
+    let id: String
+    var profilePic: UIImage
+    
+    init(name: String, email: String, id: String, profilePic: UIImage) {
+        self.name = name
+        self.email = email
+        self.id = id
+        self.profilePic = profilePic
+    }
+}
