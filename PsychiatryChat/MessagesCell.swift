8edb92954270231e117a6b85287c72f4e5419a94
雨夜 <kiras0518@gmail.com>
2019-03-08 16:02:35 +0800

    fix MessageCell

--- PsychiatryChat/MessagesCell.swift
+++ PsychiatryChat/MessagesCell.swift
@@ -10,10 +10,8 @@ import Foundation
 import UIKit
 
 class MessageCell: UITableViewCell {
-    
     @IBOutlet weak var profileImage: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var timeLabel: UILabel!
-    
 }
