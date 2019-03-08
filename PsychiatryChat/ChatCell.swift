b1e57915388c2e5f7fbcc4cc7a2f32620e75c11b
雨夜 <kiras0518@gmail.com>
2019-03-08 16:01:59 +0800

    fix SenderCell

---
+++ PsychiatryChat/ChatCell.swift
@@ -0,0 +1,14 @@
+//
+//  ChatCell.swift
+//  PsychiatryChat
+//
+//  Created by YU on 2019/3/7.
+//  Copyright © 2019 ameyo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SenderCell: UITableViewCell {
+    @IBOutlet weak var message: UILabel!
+}
