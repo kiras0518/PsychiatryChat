50872d2359ac580cb26d3a4b0da98391677d1d7a
雨夜 <kiras0518@gmail.com>
2019-04-07 22:14:42 +0800

    init MBProgressHUD 和 新增使用者的登入狀態

--- PsychiatryChat/LoginViewController.swift
+++ PsychiatryChat/LoginViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import MBProgressHUD
 
 class LoginViewController: UIViewController {
 
@@ -18,8 +19,25 @@ class LoginViewController: UIViewController {
         exitBarButton()
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        Auth.auth().addStateDidChangeListener { auth, user in
+            if user != nil {
+                print("User is signed in. Show home screen")
+                // User is signed in. Show home screen
+                let tabBarVC = self.storyboard?.instantiateViewController(withIdentifier: "segueToTabBarController")
+                self.present(tabBarVC!, animated: true, completion: nil)
+            } else {
+                // No User is signed in. Show user the login screen
+                print("No User is signed in")
+            }
+        }
+    }
     @IBAction func login(_ sender: Any) {
         loginUser()
+    }
+
+    @IBAction func forgotPasswordTapped(_ sender: Any) {
+        forgetPasswordWithEmail()
     }
 
     func loginUser() {
@@ -36,8 +54,39 @@ class LoginViewController: UIViewController {
                 self.present(alert, animated: true, completion: nil)
             }
         })
+        self.showHUD(progressLabel: "Loading...")
+        DispatchQueue.main.async {
+            self.dismissHUD(isAnimated: true)
+        }
     }
 
+    func forgetPasswordWithEmail() {
+        let forgotPasswordAlert = UIAlertController(title: "Forgot password?", message: "Enter email address", preferredStyle: .alert)
+        forgotPasswordAlert.addTextField { (textField) in
+            textField.placeholder = "Enter email address"
+        }
+        forgotPasswordAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        forgotPasswordAlert.addAction(UIAlertAction(title: "Reset Password", style: .default, handler: { (action) in
+            let resetEmail = forgotPasswordAlert.textFields?.first?.text
+            Auth.auth().sendPasswordReset(withEmail: resetEmail!, completion: { (error) in
+                //Make sure you execute the following code on the main queue
+                DispatchQueue.main.async {
+                    //Use "if let" to access the error, if it is non-nil
+                    if let error = error {
+                        let resetFailedAlert = UIAlertController(title: "Reset Failed", message: error.localizedDescription, preferredStyle: .alert)
+                        resetFailedAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                        self.present(resetFailedAlert, animated: true, completion: nil)
+                    } else {
+                        let resetEmailSentAlert = UIAlertController(title: "Reset email sent successfully", message: "Check your email", preferredStyle: .alert)
+                        resetEmailSentAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                        self.present(resetEmailSentAlert, animated: true, completion: nil)
+                    }
+                }
+            })
+        }))
+        self.present(forgotPasswordAlert, animated: true, completion: nil)
+    }
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
@@ -61,4 +110,14 @@ class LoginViewController: UIViewController {
         self.dismiss(animated: true, completion: nil)
     }
 
+}
+
+extension UIViewController {
+    func showHUD(progressLabel: String) {
+        let progressHUD = MBProgressHUD.showAdded(to: self.view, animated: true)
+        progressHUD.label.text = progressLabel
+    }
+    func dismissHUD(isAnimated: Bool) {
+        MBProgressHUD.hide(for: self.view, animated: isAnimated)
+    }
 }
